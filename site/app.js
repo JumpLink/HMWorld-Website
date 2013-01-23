902d7b384ebf3048fc6f5639fdb24a87b39f7d29
Pascal Garber <jumplink@gmail.com>
2013-01-23 16:00:29 +0100

    new branch with angular-express-seed

--- site/app.js
+++ site/app.js
@@ -1,51 +1,48 @@
+
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , fs = require('fs')
-  , hmwd = require('./hmwd.js')
-  , conf = require('./config/config.js')
-  ;
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  fs = require('fs');
 
-hmwd.init();
+var app = module.exports = express();
 
-var data_route = require('./routes/data.js')(hmwd);
-var app = express();
+// Configuration
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon(__dirname + '/public/favicon.ico'));
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/map', data_route.map_index);
-app.get('/maptest', data_route.map_test);
-app.get('/map/:name', data_route.map_info);
-app.get('/map/full/:name', data_route.map);
-app.get('/map/:mapname/layername/:layername/tile/:x/:y', data_route.maptile);
-app.get('/tileset', data_route.tileset_index);
-app.get('/tilesetid/:id', data_route.tileset_id);
-app.get('/spriteset', data_route.spriteset_index);
-app.get('/spriteset/:spriteset_name/:layer_index', data_route.spriteset_layer);
-app.get('/dialog', routes.work);
-app.get('/dev', routes.work);
+// Routes
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
--- site/routes/index.js
+++ site/routes/index.js
@@ -2,37 +2,12 @@
 /*
  * GET home page.
  */
-var Showdown = require('showdown');
-var converter = new Showdown.converter();
-var fetchUrl = require("fetch").fetchUrl;
-
-function get_readme(return_readme) {
-  fetchUrl("https://raw.github.com/JumpLink/HMWorld/master/README.md", function(error, meta, body){
-    cb(converter.makeHtml(body.toString()));
-  });
-}
-
-function return_readme(html) {
-  return html;
-}
-
 
 exports.index = function(req, res){
-  fetchUrl("https://raw.github.com/JumpLink/HMWorld/master/README.md", function(error, meta, body){
-    var readme;
-    if(typeof(body)!=="undefined")
-      readme = converter.makeHtml(body.toString());
-    else
-      readme = "coudn't load README.md";
-    res.render('index', {
-      title: 'HMWorld',
-      readme:readme
-    });
-  });
+  res.render('index');
 };
 
-exports.work = function(req, res){
-  res.render('work', {
-    title: 'HMWorld - work in progress'
-  });
+exports.partials = function (req, res) {
+  var name = req.params.name;
+  res.render('partials/' + name);
 };
